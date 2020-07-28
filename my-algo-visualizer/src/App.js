f506860ece7e8e2224f432a01d3130d4cc317b2e
Adam Chois <dragonboy174@gmail.com>
2020-07-27 20:05:40 -0700

    starting data visualization

---
+++ my-algo-visualizer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SortingVisualizer from './sortingvisualizer/SortingVisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
